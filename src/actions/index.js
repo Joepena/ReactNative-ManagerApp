e142643fcf4f2f63cb488e1fd865416f59871176
josephpendev@gmail.com <josephpendev@gmail.com>
2017-01-14 22:15:49 -0500

    employeeUpdate action and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from './types';
+import { 
+    EMAIL_CHANGED, 
+    PASSWORD_CHANGED, 
+    LOGIN_USER_SUCCESS, 
+    LOGIN_USER_FAIL, 
+    LOGIN_USER } from './types';
 
 export const emailChanged = (text) => {
     return {
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+    
+    
     render() {
         return (
             <Card>
