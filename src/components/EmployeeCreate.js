ffa36b5ecb988252cba30d73d2fdf23d57b88200
josephpendev@gmail.com <josephpendev@gmail.com>
2017-01-16 19:13:04 -0500

    picker styles

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -28,7 +28,12 @@ class EmployeeCreate extends Component {
                     />
                 </CardSection>
 
-                <CardSection>
+                <CardSection >
+                    <Text
+                        style={styles.pickerTextStyle}
+                    >
+                        Shift
+                    </Text>
                     <Picker
                         style={{ flex: 1 }}
                         selectedValue={this.props.shift}
@@ -51,6 +56,13 @@ class EmployeeCreate extends Component {
         );
     }
 }
+
+const styles = {
+    pickerTextStyle: {
+        fontSize: 18,
+        paddingLeft: 20
+    }
+};
 
 const mapStateToProps = (state) => {
     const { name, phone, shift } = state.employeeForm;
