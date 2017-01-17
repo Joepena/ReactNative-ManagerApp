8163d6f43b63f077b65d9d0c0f6414a34f2f19da
josephpendev@gmail.com <josephpendev@gmail.com>
2017-01-17 18:25:20 -0500

    data store creation on lifecycle calls

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { View, Text, ListView } from 'react-native';
+import _ from 'lodash';
 import { employeesFetch } from '../actions';
 
-import { View, Text } from 'react-native';
 
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeesFetch();
+        this.createDataSource(this.props);     
     }
+
+    componentWillRecieveProps(nextProps) {
+        //nextProps are the next set of props that this component will be rendered with.
+        //this.props is still the old set of props
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees }) {
+         const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(employees);
+    }
+
     render() {
         return (
             <View>
@@ -23,4 +40,12 @@ class EmployeeList extends Component {
     }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = (state) => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid };
+    });
+
+    return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
