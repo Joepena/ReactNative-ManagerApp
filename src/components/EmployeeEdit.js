db546228d57e6e64b983df329328ac9d07c33303
josephpendev@gmail.com <josephpendev@gmail.com>
2017-01-18 12:14:57 -0500

    actions creator employeeSave

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
 
@@ -9,7 +9,7 @@ class EmployeeEdit extends Component {
     onButtonPress() {
         const { name, phone, shift } = this.props;
 
-        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
     }
     
     componentWillMount() {
@@ -37,4 +37,4 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
