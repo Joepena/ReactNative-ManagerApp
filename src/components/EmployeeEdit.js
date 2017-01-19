258c2f8302f8e68fe47fc4e1f501b1f2c650a128
josephpendev@gmail.com <josephpendev@gmail.com>
2017-01-18 20:23:59 -0500

    common modal component

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
+import Communications from 'react-native-communications';
 import { employeeUpdate, employeeSave } from '../actions';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
-    onButtonPress() {
-        const { name, phone, shift } = this.props;
-
-        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
-    }
     
     componentWillMount() {
         _.each(this.props.employee, (value, prop) => {
             this.props.employeeUpdate({ prop, value });
         });
+    }
+    
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+
+        this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
+    }
+    onTextPress() {
+        const { phone, shift } = this.props;
+        
+        Communications.text(phone, `Your upcoming shift is on ${shift}`);
     }
     
     render() {
@@ -24,6 +31,10 @@ class EmployeeEdit extends Component {
                 <EmployeeForm {...this.props} />
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>Save Changes</Button>
+                </CardSection>
+                
+                 <CardSection>
+                    <Button onPress={this.onTextPress.bind(this)}>Text Schedule</Button>
                 </CardSection>
             </Card>
         );
